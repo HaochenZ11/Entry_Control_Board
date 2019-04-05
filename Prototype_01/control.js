9d72e0a5e6eb1928070825d02bda429cde4c4220
Aman Bhargava <amanb2000@gmail.com>
2019-04-05 04:04:57 -0400

    Add first barebones workable prototype

--- Prototype_01/control.js
+++ Prototype_01/control.js
@@ -1,9 +1,9 @@
 app.controller("control", function($scope) {
   $scope.firefighters = [
-    {name:"A", location:0, assignment:0},
-    {name:"B", location:0, assignment:0},
-    {name:"C", location:0, assignment:0},
-    {name:"D", location:0, assignment:0}
+    {name:"A", location:"--UNASSIGNED--", assignment:"--UNASSIGNED--"},
+    {name:"B", location:"--UNASSIGNED--", assignment:"--UNASSIGNED--"},
+    {name:"C", location:"--UNASSIGNED--", assignment:"--UNASSIGNED--"},
+    {name:"D", location:"--UNASSIGNED--", assignment:"--UNASSIGNED--"}
   ];
   $scope.locations = [
     "--UNASSIGNED--",
@@ -12,12 +12,12 @@ app.controller("control", function($scope) {
     "FAR BEDROOM",
     "TOP FLOOR",
     "BACK YARD"
-  ]
+  ];
   $scope.assignments = [
     "--UNASSIGNED--",
     "SEARCH",
     "RESCUE",
     "FIRE ATTACK",
     "VENTILLATION"
-  ]
+  ];
 });
