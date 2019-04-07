d4fe20db206398e004f20ede10ec3d42829b1dd1
Aman Bhargava <amanb2000@gmail.com>
2019-04-06 20:24:45 -0400

    Finalized v3.2 with rudimentary timing for checking in, color coding

--- Prototype_02/control.js
+++ Prototype_02/control.js
@@ -1,7 +1,8 @@
 app.controller("control", function($scope, $interval) {
 
-  $scope.timeTiers = [
-
+  $scope.timeTiers = [//currently in seconds
+    5,
+    10
   ]
 
   $scope.firefighters = [
@@ -37,10 +38,18 @@ app.controller("control", function($scope, $interval) {
   }
 
   $scope.get_color = function(firefighter){
-    if(firefighter.lastCheckIn > 100){
-      return false;
+    let delta_T = Date.now()-firefighter.lastCheckIn;
+    delta_T = new Date(delta_T)
+    let totalSeconds = delta_T.getSeconds() + 60*delta_T.getMinutes();
+    // console.log("Delta_T: " + delta_T);
+    // console.log("Delta_T type: " + typeof(delta_T));
+    if(totalSeconds > $scope.timeTiers[1]){
+      return "Red";
     }
-    return true;
+    else if(totalSeconds > $scope.timeTiers[0]){
+      return "Orange";
+    }
+    return false;
   }
 
 });
