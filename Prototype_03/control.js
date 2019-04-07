698230bbd84f4f55f58c4d3db18813d64b69b0db
Aman Bhargava <amanb2000@gmail.com>
2019-04-06 21:31:19 -0400

    Implement add/delete firefighting teams, timing for in/out of fire

--- Prototype_03/control.js
+++ Prototype_03/control.js
@@ -1,15 +1,22 @@
 app.controller("control", function($scope, $interval) {
 
-  $scope.timeTiers = [//currently in seconds
+  $scope.timeTiersCheckIn = [//currently in seconds
     5,
     10
   ]
 
+  $scope.timeTiersInFire = [//currently in seconds
+    30,
+    60
+  ]
+
+  $scope.addTeamName = "";
+
   $scope.firefighters = [
-    {name:"A", location:"--UNASSIGNED--", assignment:"--UNASSIGNED--", lastCheckIn:Date.now()},
-    {name:"B", location:"--UNASSIGNED--", assignment:"--UNASSIGNED--", lastCheckIn:Date.now()},
-    {name:"C", location:"--UNASSIGNED--", assignment:"--UNASSIGNED--", lastCheckIn:Date.now()},
-    {name:"D", location:"--UNASSIGNED--", assignment:"--UNASSIGNED--", lastCheckIn:Date.now()}
+    {name:"A", location:"--UNASSIGNED--", assignment:"--UNASSIGNED--", lastCheckIn:false, timeIn:false},
+    {name:"B", location:"--UNASSIGNED--", assignment:"--UNASSIGNED--", lastCheckIn:false, timeIn:false},
+    {name:"C", location:"--UNASSIGNED--", assignment:"--UNASSIGNED--", lastCheckIn:false, timeIn:false},
+    {name:"D", location:"--UNASSIGNED--", assignment:"--UNASSIGNED--", lastCheckIn:false, timeIn:false}
   ];
   $scope.locations = [
     "--UNASSIGNED--",
@@ -31,25 +38,98 @@ app.controller("control", function($scope, $interval) {
     $scope.now = Date.now();
   }
   tick();
-  $interval(tick, 1000);
+  $interval(tick, 200);
 
   $scope.get_true = function(){
     return true;
   }
 
-  $scope.get_color = function(firefighter){
+  $scope.getColorCheckIn = function(firefighter){
+    if(firefighter.lastCheckIn === false){
+      return "White";
+    }
     let delta_T = Date.now()-firefighter.lastCheckIn;
     delta_T = new Date(delta_T)
     let totalSeconds = delta_T.getSeconds() + 60*delta_T.getMinutes();
-    // console.log("Delta_T: " + delta_T);
-    // console.log("Delta_T type: " + typeof(delta_T));
-    if(totalSeconds > $scope.timeTiers[1]){
+    if(totalSeconds > $scope.timeTiersCheckIn[1]){
       return "Red";
     }
-    else if(totalSeconds > $scope.timeTiers[0]){
+    else if(totalSeconds > $scope.timeTiersCheckIn[0]){
       return "Orange";
     }
     return false;
+  }
+
+  $scope.getColorInFire = function(firefighter){
+    if(firefighter.timeIn === false){
+      return "White";
+    }
+    let delta_T = Date.now()-firefighter.timeIn;
+    delta_T = new Date(delta_T)
+    let totalSeconds = delta_T.getSeconds() + 60*delta_T.getMinutes();
+    if(totalSeconds > $scope.timeTiersInFire[1]){
+      return "Red";
+    }
+    else if(totalSeconds > $scope.timeTiersInFire[0]){
+      return "Orange";
+    }
+    return false;
+
+  }
+
+  $scope.checkInOrOut = function(firefighter){
+    if(firefighter.timeIn){
+      firefighter.timeIn = false;
+      firefighter.lastCheckIn = false;
+      return;
+    }
+    firefighter.timeIn = $scope.now;
+    firefighter.lastCheckIn = $scope.now;
+  }
+
+  $scope.getSignInOutText = function(firefighter){
+    if(firefighter.timeIn){
+      return ("EXIT")
+      // firefighter.timeIn = false;
+      // firefighter.lastCheckIn = false;
+    }
+    return("ENTER");
+  }
+
+  $scope.getTimeInFire = function(firefighter){
+    if(!firefighter.timeIn){
+      return "";
+    }
+    return $scope.now - firefighter.timeIn;
+  }
+
+  $scope.getTimeSinceCheckIn = function(firefighter){
+    if(!firefighter.lastCheckIn){
+      return "";
+    }
+    return $scope.now - firefighter.lastCheckIn;
+  }
+
+  $scope.deleteFirefighter = function(firefighters, i){
+    firefighters.splice(i, 1);
+  }
+
+  $scope.isValidName = function(teamName){
+    if(teamName === "" || $scope.firefighters.indexOf(teamName) != -1){
+      return false;
+    }
+    return true;
+  }
+
+  $scope.addTeam = function(teamName){
+    $scope.firefighters.push({
+      name:teamName,
+      location:"--UNASSIGNED--",
+      assignment:"--UNASSIGNED--",
+      lastCheckIn:false,
+      timeIn:false
+    });
+    return true;
   }
 
 });
